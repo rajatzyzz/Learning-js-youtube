b1f96baed51b8ba02123088f188a6968f84702db
Rajat Saini <123829308+rajatzyzz@users.noreply.github.com>
2025-04-19 08:52:53 +0000

    datatypes primitive & non.

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+//  Primitive  (call by value)
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n n rep. big int
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects (Rt object), Functions (return type obj func)
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
