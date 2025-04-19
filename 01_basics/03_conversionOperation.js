e7ec1ca35d4b68e796f248eece55284b5a639fae
Rajat Saini <123829308+rajatzyzz@users.noreply.github.com>
2025-04-19 06:57:56 +0000

    conversions of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,45 @@
+// let score = "33"
+
+// console.log(typeof score);
+
+// let valueInNumbers=Number(score)
+// console.log(typeof score);
+// console.log(typeof valueInNumbers);
+// console.log(valueInNumbers);
+
+// let scores = "abc33"
+// console.log(typeof score);
+// let valueInNumber=Number(scores)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let scor = null
+// console.log(typeof scor);
+// let value=Number(scor)
+// console.log(typeof value);
+// console.log(value);
+
+
+// let sco = undefined
+// console.log(typeof sco)
+// let values=Number(sco)
+// console.log(typeof values);
+// console.log(values);
+
+
+// /*
+// "33" => 33
+// "33ans"=> NaN
+// true =>1 , false=>0
+
+// 1= true , 0 = false
+// " " => false
+// " rajat " => true
+// */
+
+let string="rajat"
+console.log(string)
+let str=Number(string)
+console.log(typeof str);
+console.log(str);
